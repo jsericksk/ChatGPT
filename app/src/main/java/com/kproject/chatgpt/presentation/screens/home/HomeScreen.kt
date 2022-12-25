ab879dfca4c180db9780c64074677ebca03d9a77
jsericksk <jsericksk@gmail.com>
2022-12-25 01:02:53 -0300

    feat: Created initial ApiKeyAlertDialog in HomeScreen

--- app/src/main/java/com/kproject/chatgpt/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/kproject/chatgpt/presentation/screens/home/HomeScreen.kt
@@ -7,26 +7,34 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kproject.chatgpt.presentation.model.RecentChat
-import com.kproject.chatgpt.presentation.screens.components.EmptyListInfo
+import androidx.compose.ui.window.Dialog
 import com.kproject.chatgpt.R
+import com.kproject.chatgpt.presentation.model.RecentChat
 import com.kproject.chatgpt.presentation.model.fakeRecentChatsList
+import com.kproject.chatgpt.presentation.screens.components.EmptyListInfo
 import com.kproject.chatgpt.presentation.screens.components.TopBar
 import com.kproject.chatgpt.presentation.theme.CompletePreview
 import com.kproject.chatgpt.presentation.theme.PreviewTheme
@@ -48,22 +56,22 @@ private fun HomeScreenContent(
 ) {
     Scaffold(
         topBar = {
-           TopBar(
-               title = stringResource(id = R.string.app_name),
-               actions = {
-                   IconButton(
-                       onClick = {
+            TopBar(
+                title = stringResource(id = R.string.app_name),
+                actions = {
+                    IconButton(
+                        onClick = {
 
-                       }
-                   ) {
-                       Icon(
-                           imageVector = Icons.Default.Settings,
-                           contentDescription = null,
-                           tint = MaterialTheme.colors.onSurface
-                       )
-                   }
-               }
-           )
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onSurface
+                        )
+                    }
+                }
+            )
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -196,6 +204,101 @@ private fun RecentChatsListItem(
     }
 }
 
+@Composable
+private fun ApiKeyAlertDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    shape: Shape = RoundedCornerShape(14.dp),
+    apiKey: String,
+    onApiKeyChange: (String) -> Unit
+) {
+    if (showDialog) {
+        var currentApiKey by rememberSaveable { mutableStateOf(apiKey) }
+
+        Dialog(
+            onDismissRequest = onDismiss,
+            content = {
+                Column(
+                    modifier = Modifier
+                        .background(
+                            color = MaterialTheme.colors.background,
+                            shape = shape
+                        )
+                        .padding(18.dp)
+                ) {
+                    // Title
+                    Text(
+                        text = stringResource(id = R.string.api_key),
+                        fontSize = 18.sp,
+                        color = MaterialTheme.colors.onPrimary,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(Modifier.height(12.dp))
+
+                    // Content
+                    TextField(
+                        value = currentApiKey,
+                        onValueChange = { value ->
+                            currentApiKey = value
+                        },
+                        textStyle = TextStyle(
+                            color = MaterialTheme.colors.onPrimary,
+                            fontSize = 18.sp
+                        ),
+                        placeholder = {
+                            Text(
+                                text = stringResource(id = R.string.insert_api_key),
+                                color = MaterialTheme.colors.secondary
+                            )
+                        },
+                        maxLines = 3,
+                        // shape = CircleShape,
+                        colors = TextFieldDefaults.textFieldColors(
+                            cursorColor = MaterialTheme.colors.onPrimary,
+                            backgroundColor = MaterialTheme.colors.onSecondary,
+                            leadingIconColor = Color.White,
+                            trailingIconColor = Color.White,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            focusedLabelColor = MaterialTheme.colors.secondary,
+                            unfocusedLabelColor = MaterialTheme.colors.onSecondary,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+
+                    Spacer(Modifier.height(12.dp))
+
+                    // Action buttons
+                    Row(
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        TextButton(onClick = onDismiss) {
+                            Text(
+                                text = stringResource(id = R.string.button_cancel).uppercase(),
+                                color = MaterialTheme.colors.secondary
+                            )
+                        }
+                        Spacer(Modifier.width(6.dp))
+                        TextButton(
+                            onClick = { onApiKeyChange.invoke(currentApiKey) },
+                            enabled = currentApiKey.isNotBlank()
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.button_save).uppercase(),
+                                color = MaterialTheme.colors.secondary
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
 @CompletePreview
 @Composable
 private fun Preview() {
@@ -206,6 +309,19 @@ private fun Preview() {
         )
         HomeScreenContent(
             homeUiState = uiState
+        )
+    }
+}
+
+@CompletePreview
+@Composable
+private fun ApiKeyAlertDialogPreview() {
+    PreviewTheme {
+        ApiKeyAlertDialog(
+            showDialog = true,
+            onDismiss = {},
+            apiKey = "skskfjjifwojewpi3jjdjd3we",
+            onApiKeyChange = {}
         )
     }
 }
