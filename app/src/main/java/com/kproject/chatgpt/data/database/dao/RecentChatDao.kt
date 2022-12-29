6ac8abbddea4436f2b9a1ad686b0213e191d106f
jsericksk <jsericksk@gmail.com>
2022-12-29 05:31:07 -0300

    feat: Created RecentChatDao

---
+++ app/src/main/java/com/kproject/chatgpt/data/database/dao/RecentChatDao.kt
@@ -0,0 +1,24 @@
+package com.kproject.chatgpt.data.database.dao
+
+import androidx.room.*
+import com.kproject.chatgpt.data.database.entity.RecentChatEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecentChatDao {
+
+    @Query("SELECT * FROM recent_chats")
+    fun getAllRecentChats(): Flow<List<RecentChatEntity>>
+
+    @Query("SELECT * FROM recent_chats WHERE chat_id = :chatId")
+    suspend fun getRecentChatById(chatId: Long): RecentChatEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addRecentChat(recentChat: RecentChatEntity)
+
+    @Update
+    suspend fun updateRecentChat(recentChat: RecentChatEntity)
+
+    @Delete
+    suspend fun deleteRecentChat(recentChat: RecentChatEntity)
+}
