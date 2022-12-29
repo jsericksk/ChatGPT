54a55d92755f07eae7dd1ce007881f03f7c30187
jsericksk <jsericksk@gmail.com>
2022-12-29 05:33:42 -0300

    feat: Created TypeConverters

---
+++ app/src/main/java/com/kproject/chatgpt/data/database/converter/TypeConverters.kt
@@ -0,0 +1,31 @@
+package com.kproject.chatgpt.data.database.converter
+
+import androidx.room.ProvidedTypeConverter
+import com.google.gson.Gson
+import com.kproject.chatgpt.commom.model.AIModelOptions
+import java.util.*
+import androidx.room.TypeConverter
+
+@ProvidedTypeConverter
+class TypeConverters(private val gson: Gson) {
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun fromAIModelOptions(aiModelOptions: AIModelOptions): String {
+        return gson.toJson(aiModelOptions)
+    }
+
+    @TypeConverter
+    fun toAIModelOptions(json: String): AIModelOptions? {
+        return gson.fromJson(json, AIModelOptions::class.java)
+    }
+}
