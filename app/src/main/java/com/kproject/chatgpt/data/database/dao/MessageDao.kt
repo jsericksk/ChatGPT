ed47779339e5be1de01f23dbf68daaf1fe612067
jsericksk <jsericksk@gmail.com>
2022-12-29 05:29:42 -0300

    feat: Created MessageDao

---
+++ app/src/main/java/com/kproject/chatgpt/data/database/dao/MessageDao.kt
@@ -0,0 +1,18 @@
+package com.kproject.chatgpt.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kproject.chatgpt.data.database.entity.MessageEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MessageDao {
+
+    @Query("SELECT * FROM messages WHERE chat_id = :chatId")
+    fun getMessagesByChatId(chatId: Long): Flow<List<MessageEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addMessage(message: MessageEntity)
+}
