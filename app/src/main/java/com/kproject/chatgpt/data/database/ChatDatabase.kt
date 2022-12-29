2c7e580d8716af3011f85798b4790430bc2bf430
jsericksk <jsericksk@gmail.com>
2022-12-29 16:32:13 -0300

    feat: Created ChatDatabase

---
+++ app/src/main/java/com/kproject/chatgpt/data/database/ChatDatabase.kt
@@ -0,0 +1,18 @@
+package com.kproject.chatgpt.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.kproject.chatgpt.data.database.dao.MessageDao
+import com.kproject.chatgpt.data.database.dao.RecentChatDao
+import com.kproject.chatgpt.data.database.entity.MessageEntity
+import com.kproject.chatgpt.data.database.entity.RecentChatEntity
+
+@Database(entities = [RecentChatEntity::class, MessageEntity::class], version = 1)
+@TypeConverters(TypeConverters::class)
+abstract class ChatDatabase : RoomDatabase() {
+
+    abstract fun recentChatDao(): RecentChatDao
+
+    abstract fun messageDao(): MessageDao
+}
