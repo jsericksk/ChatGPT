d9767dddcf4e9f5d1c01168987a90f78b16e7317
jsericksk <jsericksk@gmail.com>
2022-12-30 02:47:39 -0300

    feat: Created MessageRepositoryImpl

--- app/src/main/java/com/kproject/chatgpt/data/repository/MessageRepositoryImpl.kt
+++ app/src/main/java/com/kproject/chatgpt/data/repository/MessageRepositoryImpl.kt
@@ -1,4 +1,27 @@
 package com.kproject.chatgpt.data.repository
 
-class MessageRepositoryImpl {
+import com.kproject.chatgpt.data.database.dao.MessageDao
+import com.kproject.chatgpt.data.mapper.fromModel
+import com.kproject.chatgpt.data.mapper.toModel
+import com.kproject.chatgpt.domain.model.MessageModel
+import com.kproject.chatgpt.domain.repository.MessageRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class MessageRepositoryImpl(
+    private val messageDao: MessageDao
+) : MessageRepository {
+
+    override fun getMessagesByChatId(chatId: Long): Flow<List<MessageModel>> {
+        return messageDao.getMessagesByChatId(chatId).map { messageListEntity ->
+            messageListEntity.map { messageEntity ->
+                messageEntity.toModel()
+            }
+        }
+    }
+
+    override suspend fun addMessage(message: MessageModel) {
+        messageDao.addMessage(message.fromModel())
+    }
+
 }
