15ba06f5b54b6cfbf24d8bad8a346ff7a9abc0ab
jsericksk <jsericksk@gmail.com>
2022-12-30 20:22:34 -0300

    refactor: Improvements and created DataModule implementation

--- app/src/main/java/com/kproject/chatgpt/data/di/DataModule.kt
+++ app/src/main/java/com/kproject/chatgpt/data/di/DataModule.kt
@@ -1,13 +1,23 @@
 package com.kproject.chatgpt.data.di
 
+import android.app.Application
 import android.content.Context
+import androidx.room.Room
+import com.kproject.chatgpt.data.api.ApiService
+import com.kproject.chatgpt.data.database.ChatDatabase
+import com.kproject.chatgpt.data.database.dao.MessageDao
+import com.kproject.chatgpt.data.database.dao.RecentChatDao
 import com.kproject.chatgpt.data.repository.PreferenceRepositoryImpl
+import com.kproject.chatgpt.data.repository.RecentChatRepositoryImpl
 import com.kproject.chatgpt.domain.repository.PreferenceRepository
+import com.kproject.chatgpt.domain.repository.RecentChatRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -16,7 +26,49 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideDataStoreRepository(@ApplicationContext applicationContext: Context): PreferenceRepository {
+    fun providePreferenceRepository(@ApplicationContext applicationContext: Context): PreferenceRepository {
         return PreferenceRepositoryImpl(applicationContext)
+    }
+
+    // Database
+    @Provides
+    @Singleton
+    fun provideChatDatabase(application: Application): ChatDatabase {
+        return Room.databaseBuilder(
+            application,
+            ChatDatabase::class.java,
+            "chat_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecentChatDao(chatDatabase: ChatDatabase): RecentChatDao {
+        return chatDatabase.recentChatDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMessageChatDao(chatDatabase: ChatDatabase): MessageDao {
+        return chatDatabase.messageDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecentChatRepository(
+        recentChatDao: RecentChatDao
+    ): RecentChatRepository {
+        return RecentChatRepositoryImpl(recentChatDao)
+    }
+
+    // API
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(ApiService.API_BASE_URL)
+            .build()
+        return retrofit.create(ApiService::class.java)
     }
 }
