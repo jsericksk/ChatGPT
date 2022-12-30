a36fcb0ad450bb01b699a6827d28ba5df4ad4e24
jsericksk <jsericksk@gmail.com>
2022-12-30 20:40:22 -0300

    feat: Created DatabaseDomainModule

---
+++ app/src/main/java/com/kproject/chatgpt/presentation/di/DatabaseDomainModule.kt
@@ -0,0 +1,66 @@
+package com.kproject.chatgpt.presentation.di
+
+import com.kproject.chatgpt.domain.repository.MessageRepository
+import com.kproject.chatgpt.domain.repository.RecentChatRepository
+import com.kproject.chatgpt.domain.usecase.api.SendMessageUseCase
+import com.kproject.chatgpt.domain.usecase.database.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseDomainModule {
+
+    // RecentMessage
+    @Provides
+    @Singleton
+    fun provideGetAllRecentChatsUseCase(recentChatRepository: RecentChatRepository): GetAllRecentChatsUseCase {
+        return GetAllRecentChatsUseCase(recentChatRepository::getAllRecentChats)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetRecentChatByIdUseCase(recentChatRepository: RecentChatRepository): GetRecentChatByIdUseCase {
+        return GetRecentChatByIdUseCase(recentChatRepository::getRecentChatById)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddRecentChatUseCase(recentChatRepository: RecentChatRepository): AddRecentChatUseCase {
+        return AddRecentChatUseCase(recentChatRepository::addRecentChat)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeleteRecentChatUseCase(recentChatRepository: RecentChatRepository): DeleteRecentChatUseCase {
+        return DeleteRecentChatUseCase(recentChatRepository::deleteRecentChat)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUpdateRecentChatUseCase(recentChatRepository: RecentChatRepository): UpdateRecentChatUseCase {
+        return UpdateRecentChatUseCase(recentChatRepository::updateRecentChat)
+    }
+
+    // Message
+    @Provides
+    @Singleton
+    fun provideAddMessageUseCase(messageRepository: MessageRepository): AddMessageUseCase {
+        return AddMessageUseCase(messageRepository::addMessage)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMessagesByChatIdUseCase(messageRepository: MessageRepository): GetMessagesByChatIdUseCase {
+        return GetMessagesByChatIdUseCase(messageRepository::getMessagesByChatId)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSendMessageUseCase(messageRepository: MessageRepository): SendMessageUseCase {
+        return SendMessageUseCase(messageRepository::sendMessage)
+    }
+}
