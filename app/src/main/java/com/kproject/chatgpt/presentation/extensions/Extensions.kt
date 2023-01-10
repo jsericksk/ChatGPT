40293ce808bac8855b5290cd15587b49cf80b81a
jsericksk <jsericksk@gmail.com>
2023-01-10 20:21:00 -0300

    refactor: Improved getFormattedDate() to return date according to locale

--- app/src/main/java/com/kproject/chatgpt/presentation/extensions/Extensions.kt
+++ app/src/main/java/com/kproject/chatgpt/presentation/extensions/Extensions.kt
@@ -1,18 +1,20 @@
 package com.kproject.chatgpt.presentation.extensions
 
 import android.net.Uri
+import android.text.format.DateFormat
 import com.google.gson.Gson
 import java.text.SimpleDateFormat
 import java.util.*
 
 fun Date.getFormattedDate(showOnlyDate: Boolean = false): String {
-    val currentLocalDay = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val localizedDateFormat = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dd/MM/yyyy")
+    val currentLocalDay = SimpleDateFormat(localizedDateFormat, Locale.getDefault())
         .format(Calendar.getInstance().time)
-    val dateDay = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(this)
+    val dateDay = SimpleDateFormat(localizedDateFormat, Locale.getDefault()).format(this)
     val dateFormat = if (currentLocalDay == dateDay) {
         "HH:mm"
     } else {
-        if (showOnlyDate) "dd/MM/yyyy" else "HH:mm (dd/MM/yyyy)"
+        if (showOnlyDate) localizedDateFormat else "HH:mm ($localizedDateFormat)"
     }
     return SimpleDateFormat(dateFormat, Locale.getDefault()).format(this)
 }
